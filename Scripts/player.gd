79c6d0f105018210da1558427e213b45b9bc7bc2
Niarb144 <teddybrian543@gmail.com>
2026-09-11 15:15:28 +0300

    01-Initial Commit

---
+++ Scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const SPEED := 250.0
+const JUMP_VELOCITY := -400.0
+const GRAVITY := 1200.0
+
+
+func _physics_process(delta: float) -> void:
+	# Gravity
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+
+	# Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Horizontal movement
+	var direction := Input.get_axis("move_left", "move_right")
+
+	if direction != 0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
