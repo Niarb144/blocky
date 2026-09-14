484767ff874a6362a904cc71e9e471fe67686c89
Niarb144 <teddybrian543@gmail.com>
2026-09-14 17:01:33 +0300

    Add level transitions

--- Scripts/exit.gd
+++ Scripts/exit.gd
@@ -2,10 +2,13 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		var level = get_parent()
+	if not body.is_in_group("player"):
+		return
 
-		if level.has_key:
-			print("EXIT UNLOCKED!")
-		else:
-			print("EXIT LOCKED - FIND THE KEY!")
+	var level = get_parent()
+
+	if level.has_key:
+		print("EXIT UNLOCKED!")
+		level.complete_level()
+	else:
+		print("EXIT LOCKED - FIND THE KEY!")
--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,20 +1,28 @@
 extends Node2D
 
 var has_key: bool = false
+var level_completing: bool = false
 
 @onready var key = $Key
 
 
 func _ready() -> void:
-	print("Level started")
-	print("Key reference: ", key)
-
-	if key != null:
-		key.collected.connect(_on_key_collected)
-	else:
-		print("ERROR: Key node was not found!")
+	key.collected.connect(_on_key_collected)
 
 
 func _on_key_collected() -> void:
 	has_key = true
 	print("LEVEL: Key collected!")
+
+
+func complete_level() -> void:
+	if level_completing:
+		return
+
+	level_completing = true
+	print("LEVEL COMPLETE!")
+	call_deferred("_load_next_level")
+
+
+func _load_next_level() -> void:
+	get_tree().change_scene_to_file("res://Scenes/level_2.tscn")
