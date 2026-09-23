60a5f4cd796d3d010b95f66fe1c6d71edceb6deb
Niarb144 <teddybrian543@gmail.com>
2026-09-23 16:42:47 +0300

    Add inspector edit for horizontal and vertical platforms

--- Scripts/horizontal_platform.gd
+++ Scripts/horizontal_platform.gd
@@ -1,30 +1,54 @@
 extends AnimatableBody2D
 
+@export_category("Movement")
 @export var speed: float = 100.0
+@export var travel_distance: float = 300.0
+@export var move_right: bool = true
+@export var wait_time: float = 0.0
 
-@onready var point_a: Marker2D = $PointA
-@onready var point_b: Marker2D = $PointB
-
-var point_a_position: Vector2
-var point_b_position: Vector2
+var start_position: Vector2
+var end_position: Vector2
 var target_position: Vector2
+
+var waiting: bool = false
+var wait_timer: float = 0.0
 
 
 func _ready() -> void:
-	point_a_position = point_a.global_position
-	point_b_position = point_b.global_position
+	start_position = global_position
 
-	target_position = point_b_position
+	var direction := 1.0 if move_right else -1.0
+
+	end_position = start_position + Vector2(
+		travel_distance * direction,
+		0.0
+	)
+
+	target_position = end_position
 
 
 func _physics_process(delta: float) -> void:
+	if waiting:
+		wait_timer -= delta
+
+		if wait_timer <= 0.0:
+			waiting = false
+
+		return
+
 	global_position = global_position.move_toward(
 		target_position,
 		speed * delta
 	)
 
 	if global_position.distance_to(target_position) < 1.0:
-		if target_position == point_b_position:
-			target_position = point_a_position
+		global_position = target_position
+
+		if target_position == end_position:
+			target_position = start_position
 		else:
-			target_position = point_b_position
+			target_position = end_position
+
+		if wait_time > 0.0:
+			waiting = true
+			wait_timer = wait_time
